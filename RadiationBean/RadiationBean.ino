1cf95418a48f371477502c136e2fbef5e5bb79d6
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2015-10-03 13:12:40 -0500

    Tweaks

--- RadiationBean/RadiationBean.ino
+++ RadiationBean/RadiationBean.ino
@@ -4,7 +4,8 @@
 #include <Wire.h>
 #include "Adafruit_FRAM_I2C.h"
 
-
+uint8_t radPin = 3;
+volatile boolean countFlag = 0;
 
 radConfig_t testUnion;
 
@@ -14,11 +15,36 @@ void setup(){
     Serial.println(F("Serial Test"));
     RadBean.begin();
     RadBean.configDump();
-    
+    pinMode(radPin, INPUT);
+    for(int i = 0; i < 4; i++){
+        blink();
+
+    }
+
 }
 
 
 void loop(){
 
+    if(digitalRead(radPin)){
+        blink();
 
+    }
+
+}
+
+void blink(){
+    Bean.setLedRed(100);
+    delayMicroseconds(100);
+    Bean.setLedRed(0);
+    delayMicroseconds(100);
+}
+
+void setCountFlag(){
+    Bean.detachChangeInterrupt(radPin);
+    if(digitalRead(radPin)){
+        countFlag = 1;
+
+    }
+    Bean.attachChangeInterrupt(radPin, setCountFlag);
 }
