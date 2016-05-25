3f534d7ee0fdea13db0df5884bee04c8e37f66db
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-05-25 18:56:50 -0500

    Definitions

---
+++ RadBit/RadBit.h
@@ -0,0 +1,18 @@
+
+#ifndef __RADLED_H__
+#define __RADLED_H__
+
+/////////////////
+// DEFINITIONS //
+/////////////////
+
+
+/** LED Definitions **/
+const unsigned int LED_PIN = 13;
+
+/** Serial Definitions **/
+const unsigned long SERIAL_BAUD_RATE = 115200;
+
+
+
+#endif
