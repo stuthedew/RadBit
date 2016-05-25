f649b3926b48662793fd8e9102839f8218a2879e
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-05-25 16:16:18 -0500

    Fresh Start

---
+++ RadBit/RadBit.ino
@@ -0,0 +1,36 @@
+
+#include "RadBit.h"
+#include <Time.h>
+#include <Wire.h>
+#include "Adafruit_FRAM_I2C.h"
+
+
+void begin(){
+    //Serial
+    Serial.begin(SERIAL_BAUD_RATE);
+
+    //TODO Bluetooth
+
+    //TODO RTC
+
+    //TODO Radiation Sensor
+
+    //TODO FRAM
+
+    //TODO LED
+
+    //TODO Power Manegment
+
+}
+
+
+void setup(){
+
+begin();
+
+}
+
+
+void loop(){
+
+}
