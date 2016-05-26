0eed2501ed2e0f94b11486a39747bfd2f998287c
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-05-25 19:09:33 -0500

    Beautify

--- RadBit/RadLED.h
+++ RadBit/RadLED.h
@@ -6,18 +6,18 @@
 
 class RadLED {
 public:
-RadLED(int ledPin=13);
+	RadLED(int ledPin=13);
 
 
-void begin( void );
-void on();
-void off();
-void blink(int bDelay, int qty );
-void setState(bool state);
+	void begin( void );
+	void on();
+	void off();
+	void blink(int bDelay, int qty );
+	void setState(bool state);
 
 private:
-const int _ledPin;
-bool _ledState;
+	const int _ledPin;
+	bool _ledState;
 
 };
 
