7696801a80cfcea16ffd758a019a3d34623e31bb
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-05-26 19:39:54 -0500

    Added virtual functions for storage

--- RadBit/RadBit.h
+++ RadBit/RadBit.h
@@ -2,6 +2,8 @@
 #ifndef __RADLED_H__
 #define __RADLED_H__
 
+#include "Arduino.h"
+
 /////////////////
 // DEFINITIONS //
 /////////////////
@@ -12,6 +14,20 @@ const unsigned int LED_PIN = 13;
 
 /** Serial Definitions **/
 const unsigned long SERIAL_BAUD_RATE = 115200;
+
+typedef struct {
+	uint32_t epoch;
+	uint16_t value;
+	uint16_t period;
+}sensor_event_t;
+
+class EventStorage {
+public:
+
+    virtual void begin()=0;
+    virtual void storeEvent( sensor_event_t e ) = 0;
+
+};
 
 
 
